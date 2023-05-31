9e72429f2ea99d1d7724c37fa1601b79c85a87ed
RusenAkbal <rusenfirat@gmail.com>
2023-05-31 14:01:50 +0300

    Update
    Some adjustments

--- hud.lua
+++ hud.lua
@@ -1,15 +1,34 @@
+local hide = {
+  ["DarkRP_LocalPlayerHUD"] = true,
+}
 
-hook.Add("HUDShouldDraw", "DisableDefaultHUD", function(name)
-    if name == "DarkRP_HUD" then
-        return false
-    end
+hook.Add("HUDShouldDraw", "HideDefaultDarkRPHud", function(name)
+  if hide[name] then return false end
 end)
 
-surface.CreateFont( "HealthBar", {
+surface.CreateFont( "infoLarge", {
   font = "Trebuchet24",
   extended = false,
-  size = 20,
-  weight = 580,
+  size = 23,
+  weight = 530,
+  blursize = 0,
+  scanlines = 0,
+  antialias = true,
+  underline = false,
+  italic = false,
+  strikeout = false,
+  symbol = false,
+  rotary = false,
+  shadow = false,
+  additive = false,
+  outline = false,
+} )
+
+surface.CreateFont( "infoSmall", {
+  font = "Trebuchet24",
+  extended = false,
+  size = 17,
+  weight = 530,
   blursize = 0,
   scanlines = 0,
   antialias = true,
@@ -24,45 +43,48 @@ surface.CreateFont( "HealthBar", {
 } )
 
 
-function DrawHUD()
+hook.Add("HUDPaint", "DrawMyHud", function()
     local ply = LocalPlayer()
 
     local max_health_Length = 290
     local max_armor_Length = 290
 
-    local health_length = ply:Health()*2.9
-    local armor_length = ply:Armor()*2.9
-
-
-    --local Avatar = vgui.Create( "AvatarImage")
-
-    --Avatar:SetSize( 80, 80 )
-    --Avatar:SetPos( 40, ScrH()-215 )
-    --Avatar:SetPlayer( LocalPlayer(), 64 )
+    local health_length = ply:Health() * 2.9
+    local armor_length = ply:Armor() * 2.9
 
     if health_length > max_health_Length then 
       health_length = max_health_Length
     end
 
-    if armor_length > max_armor_Length then 
-      armor_length = max_armor_Length
-    end
+    --local Avatar = vgui.Create("AvatarImage")
 
-    draw.RoundedBox(6, 30, ScrH()-225, 312, 180, Color(0, 0, 0, 255))
+    --Avatar:SetSize(80, 80)
+    --Avatar:SetPos(40, ScrH() - 200)
+    --Avatar:SetPlayer(ply, 64)
 
-    --draw.RoundedBox(4, 40, ScrH()-215, 90, 90, Color(255, 255, 255, 255))
+    draw.RoundedBox(6, ScrW() - 1900, ScrH() - 210, 312, 180, Color(20, 20, 20, 255)) -- main frame
 
-    --draw.RoundedBox(6, 40, ScrH()-95, 290, 10, Color(255, 255, 255, 255))
-
-    --draw.RoundedBox(6, 40, ScrH()-75, 290, 10, Color(255, 255, 255, 255))
-
-    draw.RoundedBox(0, 40, ScrH()-80, health_length, 20, Color(220, 45, 45, 255))-- Health bar 
-    draw.DrawText(tostring(ply:Health()),"HealthBar", 185, ScrH()-82, Color(255,255,255), TEXT_ALIGN_CENTER) -- Health text
+    draw.RoundedBox(0, ScrW() - 1892, ScrH() - 202, 84, 84, Color(90, 90, 90, 255)) -- outline of the avatar
     
-    draw.RoundedBox(0, 40, ScrH()-58, armor_length, 5, Color(0, 130, 255, 255)) -- Armor bar
+    if ply:Armor() > 0 then 
+      draw.RoundedBox(0, ScrW()-1891, ScrH()-44, 292, 7, Color(90, 90, 90, 255)) -- outline of the armor bar
+      draw.RoundedBox(0, ScrW() - 1890, ScrH() - 43, health_length, 5, Color(0, 130, 255, 255)) -- health bar -- Armor bar
+      if armor_length > max_armor_Length then 
+        armor_length = max_armor_Length
+      end
+    end 
 
-end
+    if string.len(ply:Name()) > 19 then
+    draw.DrawText(ply:Name(), "infoSmall", ScrW() - 1800, ScrH() - 203, Color(255, 255, 255), TEXT_ALIGN_LEFT) -- name (resized)
+    else 
+    draw.DrawText(ply:Name(), "infoLarge", ScrW() - 1800, ScrH() - 203, Color(255, 255, 255), TEXT_ALIGN_LEFT) -- name (original size)
+    end 
 
+    draw.DrawText("Job: "..ply:getDarkRPVar( "job" ), "infoLarge", ScrW() - 1800, ScrH() - 180, Color(255, 255, 255), TEXT_ALIGN_LEFT) -- job 
+    draw.DrawText("Salary: "..ply:getDarkRPVar( "salary" ), "infoLarge", ScrW() - 1800, ScrH() - 160, Color(255, 255, 255), TEXT_ALIGN_LEFT) -- salary
 
-hook.Add("HUDPaint", "CustomHUD", DrawHUD) -- Calling the DrawHUD
+    draw.RoundedBox(0, ScrW() - 1891, ScrH() - 66, 292, 22, Color(90, 90, 90, 255)) -- outline of the health bar
+    draw.RoundedBox(0, ScrW() - 1890, ScrH() - 65, health_length, 20, Color(220, 45, 45, 255)) -- health bar
+    draw.DrawText(tostring(ply:Health()), "infoLarge", ScrW() - 1745, ScrH() - 67, Color(255, 255, 255), TEXT_ALIGN_CENTER) -- health text
 
+end)
