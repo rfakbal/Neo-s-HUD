b79d8a6075d1bb68a71b7c69c5964f72cc623519
RusenAkbal <rusenfirat@gmail.com>
2023-06-03 23:22:05 +0300

    Update
    Added/edited some commits

--- hud.lua
+++ hud.lua
@@ -88,16 +88,16 @@ local cash_icon = Material( "icon16/money.png" )
 local haveArmor = false
 
 if ply:Armor() > 0 then haveArmor = true end
-local Avatar = vgui.Create("AvatarImage")
+local Avatar = vgui.Create("AvatarImage") 
 
 Avatar:SetSize(80, 80)
-Avatar:SetPos(ScrW() - 1890, ScrH() - 200)
+Avatar:SetPos(ScrW() - 1890, ScrH() - 200) 
 Avatar:SetPlayer(ply, 64)
 
 local main_frame_H = 155
 
 if haveArmor then
-   main_frame_H = 180
+   main_frame_H = 180 -- edits main frame's height depends on the haveArmor condition 
    if armor_length > max_armor_Length then
       armor_length = max_armor_Length
    end
@@ -105,23 +105,23 @@ end
 
 draw.RoundedBox(6, ScrW() - 245, ScrH() - 100, 155, 90, Color(30, 30, 30, 255)) -- ammo frame
 
-if ply:GetActiveWeapon() != NULL then
+if ply:GetActiveWeapon() != NULL then -- checks if player got any weapon 
    local ammo_in_clip -- ammo value of the current mag/clip
    local clip_amount -- ammo value of the total mags/clips without current mag
 
-   if ply:GetActiveWeapon():Clip1() < 0 then -- If the ammo is LEFF THAN 0 (Infinite) then do......
-      ammo_in_clip = "∞" -- Displays infinite sign
-      clip_amount = ""
+   if ply:GetActiveWeapon():Clip1() < 0 then -- if the ammo is LEFF THAN 0 (Infinite) then do......
+      ammo_in_clip = "∞" -- displays infinite sign
+      clip_amount = ""  
    elseif ply:GetActiveWeapon():Clip1() > -1 then
       ammo_in_clip = ply:GetActiveWeapon():Clip1()
       clip_amount = "/"..ply:GetAmmoCount( ply:GetActiveWeapon():GetPrimaryAmmoType() )
    end
 
-   draw.DrawText( ammo_in_clip..clip_amount ,"infoXLarge", ScrW() - 167, ScrH() - 78, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER ) -- Displays ammo in and out of mag with "/"
+   draw.DrawText( ammo_in_clip..clip_amount ,"infoXLarge", ScrW() - 167, ScrH() - 78, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER ) -- displays ammo in and out of mag with "/"
 
 else
 
-   draw.DrawText( "You are dead" ,"infoXLarge", ScrW() - 245, ScrH() - 60, Color( 255, 255, 255 ) ) -- Displays you are dead if you are dead
+   draw.DrawText( "You are dead" ,"infoXLarge", ScrW() - 167, ScrH() - 78, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER ) -- displays you are dead if you are dead
 end
 
 
@@ -131,7 +131,7 @@ draw.RoundedBox(0, ScrW() - 1893, ScrH() - 203, 86, 86, Color(90, 90, 90, 255)) 
 
 if haveArmor then
    draw.RoundedBox(0, ScrW()-1892, ScrH()-60, 294, 24, Color(90, 90, 90, 255)) -- outline of the armor bar
-   draw.RoundedBox(0, ScrW() - 1890, ScrH() - 58, armor_length, 20, Color(0, 130, 255, 255))  -- Armor bar
+   draw.RoundedBox(0, ScrW() - 1890, ScrH() - 58, armor_length, 20, Color(0, 130, 255, 255))  -- armor bar
    draw.DrawText(tostring(ply:Armor()), "infoXLarge", ScrW() - 1745, ScrH() - 60, Color(255, 255, 255), TEXT_ALIGN_CENTER) -- health text
 end
 
@@ -148,7 +148,7 @@ draw.RoundedBox(0, ScrW() - 1892, ScrH() - 97, 294, 29, Color(90, 90, 90, 255)) 
 draw.RoundedBox(0, ScrW() - 1890, ScrH() - 95, health_length, 25, Color(220, 45, 45, 255)) -- health bar
 draw.DrawText(tostring(ply:Health()), "infoLarge", ScrW() - 1745, ScrH() - 95, Color(255, 255, 255), TEXT_ALIGN_CENTER) -- health text
 
-DrawIcon(cash_icon, ScrW() - 1802, ScrH() - 132, 15, 15, Color(255,255,255,255))
+DrawIcon(cash_icon, ScrW() - 1802, ScrH() - 132, 15, 15, Color(255,255,255,255)) -- displaying cash icon 
 
 
 end)
