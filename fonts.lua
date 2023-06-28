49643987f77af15336f9e1d0fa4d151e50469fa0
RusenAkbal <rusenfirat@gmail.com>
2023-06-28 23:56:33 +0300

    Update

---
+++ fonts.lua
@@ -0,0 +1,56 @@
+if CLIENT then 
+   surface.CreateFont( "infoXLarge", {
+   font = "Trebuchet24",
+   extended = false,
+   size = 40,
+   weight = 600,
+   blursize = 0,
+   scanlines = 0,
+   antialias = true,
+   underline = false,
+   italic = false,
+   strikeout = false,
+   symbol = false,
+   rotary = false,
+   shadow = false,
+   additive = false,
+   outline = false,
+} )
+
+surface.CreateFont( "infoLarge", {
+   font = "Trebuchet24",
+   extended = false,
+   size = 23,
+   weight = 530,
+   blursize = 0,
+   scanlines = 0,
+   antialias = true,
+   underline = false,
+   italic = false,
+   strikeout = false,
+   symbol = false,
+   rotary = false,
+   shadow = false,
+   additive = false,
+   outline = false,
+} )
+
+surface.CreateFont( "infoSmall", {
+   font = "Trebuchet24",
+   extended = false,
+   size = 17,
+   weight = 530,
+   blursize = 0,
+   scanlines = 0,
+   antialias = true,
+   underline = false,
+   italic = false,
+   strikeout = false,
+   symbol = false,
+   rotary = false,
+   shadow = false,
+   additive = false,
+   outline = false,
+} )
+
+end
--- hud.lua
+++ hud.lua
@@ -1,3 +1,4 @@
+include("fonts.lua")
 local hide = {
    ["DarkRP_LocalPlayerHUD"] = true,
    ["CHudDeathNotice"] = true,
@@ -30,61 +31,9 @@ local main_frame_color = Color(30, 30, 30, 255)
 
 local enableOutline = false
 
-   hook.Add("HUDPaint", "DrawMyHud", function()
+
+hook.Add("HUDPaint", "DrawMyHud", function()
    
-surface.CreateFont( "infoXLarge", {
-   font = "Trebuchet24",
-   extended = false,
-   size = 40,
-   weight = 600,
-   blursize = 0,
-   scanlines = 0,
-   antialias = true,
-   underline = false,
-   italic = false,
-   strikeout = false,
-   symbol = false,
-   rotary = false,
-   shadow = false,
-   additive = false,
-   outline = false,
-} )
-
-surface.CreateFont( "infoLarge", {
-   font = "Trebuchet24",
-   extended = false,
-   size = 23,
-   weight = 530,
-   blursize = 0,
-   scanlines = 0,
-   antialias = true,
-   underline = false,
-   italic = false,
-   strikeout = false,
-   symbol = false,
-   rotary = false,
-   shadow = false,
-   additive = false,
-   outline = false,
-} )
-
-surface.CreateFont( "infoSmall", {
-   font = "Trebuchet24",
-   extended = false,
-   size = 17,
-   weight = 530,
-   blursize = 0,
-   scanlines = 0,
-   antialias = true,
-   underline = false,
-   italic = false,
-   strikeout = false,
-   symbol = false,
-   rotary = false,
-   shadow = false,
-   additive = false,
-   outline = false,
-} )
 
    local ply = LocalPlayer()
    
